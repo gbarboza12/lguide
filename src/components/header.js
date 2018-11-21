343b8daa607b0a8221b78d361e41e552ee4c33f0
Lupe <gbarboza1231@gmail.com>
2018-11-21 13:03:40 -0600

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Header = ({ siteTitle }) => (
+  <div className="app-header">
+    <nav className="navigation-bar">
+      <div>
+        <ul className="navigation__list">
+          <li>
+            <Link className="navigation__link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="navigation__link" to="/">
+              Books
+            </Link>
+          </li>
+          <li>
+            <Link className="navigation__link" to="/">
+              Articles
+            </Link>
+          </li>
+          <li>
+            <Link className="navigation__link" to="/">
+              Film
+            </Link>
+          </li>
+          <li>
+            <Link className="navigation__link" to="/">
+              Social Media
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+)
+
+export default Header
