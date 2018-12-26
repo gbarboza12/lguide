76c3971f738478c3189e2996788558e4149b0dcc
gbarboza12 <gbarboza1231@gmail.com>
2018-12-26 14:24:27 -0800

    topics list functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
+import _ from "lodash";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import Layout from '../components/layout';
 
-const IndexPage = () => (
-  <Layout>
-    <div className="container-fluid main-container">
-      <div className="form-group search-content">
-        <input type="search" class="form-control" id="search" placeholder="Search" />
-        <span className="icon"><FontAwesomeIcon icon={faSearch} /></span>
-      </div>
-      <div className="topics-content">
-        <h3>Topics</h3> 
-        <br />
-      </div>
-    </div>
+export default class IndexPage extends Component {
+  getTags() {
+    const tags = [];
+    this.props.data.allMarkdownRemark.group.forEach(tag => {
+      tags.push({
+        tagName: tag.fieldValue.charAt(0).toUpperCase() + tag.fieldValue.slice(1),
+        count: tag.totalCount
+      });
+    });
+    return tags;
+  }
 
-  </Layout>
-)
+  render() {
+    const tagsList = this.getTags();
+    return (
+      <Layout>
+        <div className="container-fluid main-container">
+          <div className="form-group search-content">
+            <input type="search" className="form-control" id="search" placeholder="Search" />
+            <span className="icon"><FontAwesomeIcon icon={faSearch} /></span>
+          </div>
+          <div className="topics-content">
+            <h3>Topics</h3>
+            {
+              tagsList.map(tag => (
+                <Link
+                  key={tag.tagName}
+                  to={`/tags/${_.kebabCase(tag.tagName)}`}
+                > {tag.tagName}({tag.count})
+              </Link>
+              ))
+            }
+            <br />
+          </div>
+        </div>
 
-export default IndexPage;
+      </Layout>
+    )
+  }
+}
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
--- src/templates/post.js
+++ src/templates/post.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import _ from "lodash";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
 import "./post.css";
 
 export default class Post extends Component {
@@ -10,20 +10,23 @@ export default class Post extends Component {
     const { post } = this.props;
 
     return (
-      <div key={post.id}>
+      <div key={post.id} className="post-div">
         <h3>
           {post.title}
         </h3>
         <p dangerouslySetInnerHTML={{ __html: post.html }} />
-        {post.tags && 
+        {post.tags ?
           post.tags.map(tag => (
-            <Link
-              key={tag}
-              to={`/tags/${_.kebabCase(tag)}`}
-              ><FontAwesomeIcon icon={faHashtag} />{tag}</Link>
-          ))
-        }
-        <hr class="post-divider"></hr>
+            <div className="text-right">
+              <Link
+                key={tag}
+                to={`/tags/${_.kebabCase(tag)}`}
+              ><FontAwesomeIcon icon={faTag} />{tag}</Link>
+            </div>
+          )
+          ) : null}
+
+        <hr className="post-divider"></hr>
       </div>
     )
   }
