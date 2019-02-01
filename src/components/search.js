a318bd2b72903f575affda2ebe744c5e785dfac9
Lupe <gbarboza1231@gmail.com>
2019-02-01 15:18:59 -0600

    search css changes

--- src/components/search.js
+++ src/components/search.js
@@ -10,7 +10,34 @@ class Search extends Component {
     this.state = {
       query: ``,
       results: [],
+      showResults: false,
+      searchFormCSS: 'search-form',
     };
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+  handleFocus() {
+    this.setState({
+      showResults: true,
+      searchFormCSS: 'search-form.focus',
+    });
+  }
+  handleBlur() {
+    this.setState({
+      // showResults: false,
+      searchFormCSS: 'search-form',
+    });
+  }
+  showResultsDiv() {
+    const showResults = this.state.showResults;
+    const results = this.state.results;
+    if (results.length === 0) {
+      return false;
+    } else if (results.length > 0 && showResults) {
+      return true;
+    } else {
+      return false;
+    }
   }
   getOrCreateIndex = () =>
     this.index ? this.index : Index.load(this.props.searchData);
@@ -19,6 +46,7 @@ class Search extends Component {
     const query = evt.target.value;
     this.index = this.getOrCreateIndex();
     this.setState({
+      showResults: true,
       query,
       results: this.index
         .search(query, { expand: true }) // Accept partial matches
@@ -27,34 +55,37 @@ class Search extends Component {
   };
 
   render() {
+    const showResultsDiv = this.showResultsDiv();
+    const resultsList = this.state.results;
+    const searchFormCSS = this.state.searchFormCSS;
     return (
-      <div className="col-md-2">
-        <form className="search-form">
-          <div className="form-group">
+      <div className="col-md-3 search-div">
+        <form className={searchFormCSS}>
+          <div className="input-group input-wrapper">
             <input
               id="search"
               type="search"
               autoComplete="off"
-              className="form-control"
+              className="form-control input-search"
               name="search"
               placeholder="Search"
               onChange={this.search}
+              onFocus={this.handleFocus}
+              onBlur={this.handleBlur}
             />
             <span className="glyphicon glyphicon-search form-control-feedback">
               <FontAwesomeIcon icon={faSearch} />
             </span>
           </div>
-          </form>
-        {this.state.results.length > 0 ? (
-          <div className="search-results-div ">
-            <ul>
-              {this.state.results.map(page => (
-                <li key={page.id}>
-                  <Link to={page.slug}>{page.title}</Link>
-                 
-                </li>
-              ))}
-            </ul>
+        </form>
+        {showResultsDiv ? (
+          <div className="search-results-div" ref={node => (this.node = node)}>
+            {resultsList.map(page => (
+              <div className="search-results-item">
+                <Link to={page.slug}>{page.title}</Link>
+                <p className="topics-text">Related topics: {page.tags.join(`, `)}</p>
+              </div>
+            ))}
           </div>
         ) : null}
       </div>
